f9c81a134d6156726678486339b69b7f5733f20d
RedKatzen <gabrieldeavilaf@gmail.com.br>
2025-03-01 12:00:21 -0300

    Validação da estrutura do email

---
+++ src/main.js
@@ -0,0 +1,22 @@
+let emailInput = document.querySelectorAll("#email");
+let errorMessage = document.querySelector(".error-message");
+
+const regexEmail = (email) => {
+  return String(email)
+    .toLocaleLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+emailInput[0].addEventListener("change", (e) => {
+  e.preventDefault();
+  let email = e.target.value;
+  let isValid = regexEmail(email);
+  if (!isValid) {
+    errorMessage.classList.remove('hidden')
+  } else {
+    errorMessage.classList.add('hidden')
+  }
+});
+
