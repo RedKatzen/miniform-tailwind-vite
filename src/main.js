f5feda640d229447300211f5e9b480c2fc1a021b
RedKatzen <gabrieldeavilaf@gmail.com.br>
2025-03-03 12:00:27 -0300

    Formatação de telefone

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-let emailInput = document.querySelectorAll("#email");
+let emailInput = document.querySelector("#email");
+let phoneInput = document.querySelector("#phone");
+let passwordInput = document.querySelector("#password");
+let confirmPwInput = document.querySelector("#confirmPw");
+
 let errorMessage = document.querySelector(".error-message");
 
 const regexEmail = (email) => {
@@ -9,14 +13,33 @@ const regexEmail = (email) => {
     );
 };
 
-emailInput[0].addEventListener("change", (e) => {
+emailInput.addEventListener("change", (e) => {
   e.preventDefault();
   let email = e.target.value;
   let isValid = regexEmail(email);
   if (!isValid) {
-    errorMessage.classList.remove('hidden')
+    errorMessage.classList.remove("hidden");
   } else {
-    errorMessage.classList.add('hidden')
+    errorMessage.classList.add("hidden");
   }
 });
 
+phoneInput.addEventListener("input", (e) => {
+  e.preventDefault();
+  e.target.value = formatPhone(e.target.value);
+});
+
+const formatPhone = (phone) => {
+  let digits = phone.replace(/\D/g, "").substring(0, 10);
+  let regionCode = digits.substring(0, 2);
+  let prefix = digits.substring(2, 6);
+  let suffix = digits.substring(6, 10);
+
+  let size = digits.length;
+
+  if (size > 6) return `(${regionCode}) ${prefix}-${suffix}`;
+  else if (size > 2) return `(${regionCode}) ${prefix}`;
+  else if (size > 0) return `(${regionCode})`;
+
+  return "";
+};
